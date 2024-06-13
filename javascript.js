a602a4fc0da67a02056bbe7c2548a65752e8e569
Auric <112778782+AuricOls@users.noreply.github.com>
2024-06-12 19:17:37 -0600

    Add feature to generate new grid

--- javascript.js
+++ javascript.js
@@ -1,5 +1,8 @@
-const parentDiv = document.getElementById("container-main");
 const GRIDSIZE = 620;
+const inContainer = document.getElementById("input-container");
+const parentDiv = document.getElementById("container-main");
+const inputDimension = document.getElementById("dimension");
+const generateButton = document.getElementById("generate");
 function createDiv(num){
     let dimension = num;
 
@@ -25,6 +28,14 @@ function randomColor(){
     return "rgb(" + Math.floor(Math.random() * 255) + ", " + Math.floor(Math.random() * 255) + ", " + Math.floor(Math.random() * 255) + ")";
 }
 
-
+generateButton.addEventListener("click", function(){
+    const gridItems = document.querySelectorAll(".grid-item");
+    gridItems.forEach(function(item) {
+        item.remove();
+    });
+    const dimension = parseInt(inputDimension.value);
+    createDiv(dimension);
+    inputDimension.value = "";
+});
 
 createDiv(16);
